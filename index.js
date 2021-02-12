b3b330895199c0ba8a4932828d526d61fd35dc03
Djescho <djescho.mananga@hva.nl>
2021-02-12 21:58:57 +0100

    added images

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//app.get kiest wat je bij welke url te zien krijgt.
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/hi', (req, res) => {
     res.send('OMG YOU FOND THE OTHER PAGE!!')
-  })
+})
 
+//app.use laat weten welke mappen er middels urls toegangelijk zijn.
+app.use(express.static('public'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`BTapp listening at http://localhost:${port}`)
 })
