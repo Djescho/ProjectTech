6c9834b08a431d2a55e6c69f283a1aa0b75c63ac
Djescho <djescho.mananga@hva.nl>
2021-02-12 21:33:50 +0100

    update index.js with first trial express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+console.log("Hello world, lets spin the server up shall we?")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/hi', (req, res) => {
+    res.send('OMG YOU FOND THE OTHER PAGE!!')
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
