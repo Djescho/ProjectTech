b71c30f82f085964b2335bcefec056b43fd69c5e
Djescho <djescho.mananga@hva.nl>
2021-02-13 00:53:48 +0100

    update

--- public/scripts/findSong.js
+++ public/scripts/findSong.js
@@ -1,8 +1,8 @@
 var Spotify = require('node-spotify-api');
- 
+let value = toString()
 var spotify = new Spotify({
-  id: 'd5e588b968774c59b1e8b2eebf2a8514',
-  secret: 'c1be4cdda8824d93a63bfacb07003537'
+  // id: 'd5e588b968774c59b1e8b2eebf2a8514',
+  // secret: 'c1be4cdda8824d93a63bfacb07003537'
 });
  
 spotify.search({ type: 'track', query: 'Wedding song wendy shay' }, function(err, data) {
@@ -13,6 +13,7 @@ spotify.search({ type: 'track', query: 'Wedding song wendy shay' }, function(err
 console.log(data)
 console.log(data.items)
 let x = data.items
-console.log(x.value)
+let y = x[0]
+console.log(toString(x))
 
 });
