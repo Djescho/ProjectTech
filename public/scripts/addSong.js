a074321ddfbbb3e7ce05913bd253be6481ff0308
Djescho <djescho.mananga@hva.nl>
2021-02-14 23:33:14 +0100

    got my first API script working

---
+++ public/scripts/addSong.js
@@ -0,0 +1,47 @@
+// let button = document.querySelector('#addSong');
+let Spotify = require('node-spotify-api');
+let ejs = require('ejs')
+let value = toString()
+
+let spotify = new Spotify({
+  id: 'd5e588b968774c59b1e8b2eebf2a8514',
+  secret: 'a secret darling <3'
+  
+});
+
+
+// console.log(process.argv);
+let inputArray = [];
+
+for (let i = 2; i <process.argv.length; i++) {
+ inputArray.push(process.argv[i])
+}
+
+let inputString = inputArray.join(' ');
+// console.log(inputString)
+
+
+// function addSong (){
+spotify.search({ type: 'track', query: inputString }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+// console.log(data)
+// console.log(data.items)
+// let x = data.tracks.items[0];
+// console.log(x)
+
+let songName = data.tracks.items[0].name
+console.log(songName)
+
+songArtistList = data.tracks.items[0].album.artists
+// console.log(songArtistList)
+
+for (let i = 0; i < songArtistList.length; i++) { 
+  console.log(songArtistList[i].name);
+}
+
+});
+// }
+// button.addEventListener('click', addSong);
