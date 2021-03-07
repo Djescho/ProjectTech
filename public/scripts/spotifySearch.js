2317660f60735558aa05bdab8b69457b63cb78f9
Djescho <djescho.mananga@hva.nl>
2021-03-07 11:16:12 +0100

    update

--- public/scripts/spotifySearch.js
+++ public/scripts/spotifySearch.js
@@ -3,37 +3,61 @@ require("dotenv").config();
 
 let spotify = new Spotify({
   id: "d5e588b968774c59b1e8b2eebf2a8514",
-  secret: process.env.SPOTAPIKEY,
+  secret: "5c182bd48b7e40b7ab1dbd1e9577d1f1",
 });
+userInput = ["wendy shay wedding song", "fantana so what", "zuchu sukari"];
+finalObject = [];
 
-// function searchSong() {
-let inputArray = [];
-for (let i = 2; i < process.argv.length; i++) {
-  inputArray.push(process.argv[i]);
+console.log(userInput.length);
+//start looping through input
+for (let i = 0; i < userInput.length; i++) {
+  console.log("itteration: " + i);
+  //start search on spotify
+  spotify.search({ type: "track", query: userInput[i] }, function (err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    //error prevention
+    if (data.tracks.total == 0) {
+      console.log("no results for " + userInput);
+    } else {
+      console.log(
+        "de zoekopdracht " + userInput[i] + " levert de volgende resultaten: "
+      );
+      console.log(data);
+
+      //aan de slag met het eerste zoekresultaat
+      let songName = data.tracks.items[0].name;
+      console.log("Track Title: " + songName);
+
+      let songArtist = [];
+      let songArtistList = data.tracks.items[0].artists;
+      for (let i = 0; i < songArtistList.length; i++) {
+        songArtist.push(songArtistList[i].name);
+      }
+
+      console.log("Artist(s): " + songArtist);
+
+      let songID = data.tracks.items[0].id;
+      console.log("Song ID: " + songID);
+
+      songObject = {
+        title: songName,
+        artists: songArtist,
+        songID: songID,
+      };
+      console.log("totale object:");
+      console.log(songObject);
+
+      finalObject.push(songObject);
+      console.log("query " + i + "has been pushed");
+    }
+    if (i === userInput.length - 1) {
+      console.log("final combined object:");
+      console.log(finalObject);
+    }
+    //sluit spotify serarch
+  });
+
+  //sluit loop
 }
-let inputString = inputArray.join(" ");
-console.log("searched for: " + inputString);
-
-//start search on spotify
-spotify.search({ type: "track", query: inputString }, function (err, data) {
-  if (err) {
-    return console.log("Error occurred: " + err);
-  }
-  console.log("de zoekopdracht levert de volgende resultaten: ");
-
-  //aan de slag met het eerste zoekresultaat
-  let songName = data.tracks.items[0].name;
-  console.log("Track Title: " + songName);
-
-  let songArtist = [];
-  let songArtistList = data.tracks.items[0].artists;
-  for (let i = 0; i < songArtistList.length; i++) {
-    songArtist.push(songArtistList[i].name);
-  }
-
-  console.log("Artist(s): " + songArtist);
-
-  let songID = data.tracks.items[0].id;
-  console.log("Song ID: " + songID);
-});
-// }
--- server.js
+++ server.js
@@ -5,8 +5,32 @@ const port = 3000;
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { MongoClient } = require("mongodb");
 
 require("dotenv").config();
+
+// const db = require("./db");
+// const dbName = "musicmatch";
+// const collectionName = "test";
+
+// db.initialize(
+//   dbName,
+//   collectionName,
+//   function (dbCollection) {
+//     // successCallback
+//     // get all items
+//     dbCollection.find().toArray(function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//     });
+
+//     // << db CRUD routes >>
+//   },
+//   function (err) {
+//     // failureCallback
+//     throw err;
+//   }
+// );
 
 app.set("view enigine", "ejs");
 app.set("views", "views");
@@ -20,21 +44,55 @@ app.use(bodyParser.json());
 //app.get kiest wat je bij welke url te zien krijgt.
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { data: { name: "Djescho" } });
+  res.render("login.ejs");
 });
-app.get("/profile", (req, res) => {
+
+app.get("/editprofile", (req, res) => {
   res.render("editprofile.ejs");
 });
-app.get("/search", (req, res) => {
-  res.render("search.ejs");
+app.get("/newprofile", (req, res) => {
+  res.render("newprofile.ejs");
 });
 
 //app.use laat weten welke mappen er middels urls toegangelijk zijn.
 
-app.post("/profile", (req, res) => {
-  console.log("something has ben submited!");
-  console.log(req.body);
-  res.render("profile.ejs", { data: req.body });
+//Wanner er een nieuw profiel wordt aangemaakt
+
+app.post("/newProfile", (req, res) => {
+  console.log("Er is een nieuw profiel toegevoegd");
+  let userProfile = req.body;
+  console.log(userProfile);
+
+  const url = process.env.DATABASEKEY;
+  const client = new MongoClient(url);
+  const dbName = "musicmatch";
+
+  async function uploadData() {
+    //informatie over try catch https://nodejs.org/en/knowledge/errors/what-is-try-catch/
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const col = db.collection("test");
+      console.log("Connected correctly to server");
+
+      const p = await col.insertOne(userProfile);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      await client.close();
+    }
+  }
+
+  uploadData().catch(console.dir);
+
+  res.render("index.ejs", { data: userProfile });
+});
+app.post("/updateProfile", (req, res) => {
+  console.log("er heeft een profielupdate plaats geveonden");
+  res.render("index.ejs");
+});
+app.post("/login", (req, res) => {
+  res.render("index.ejs");
 });
 
 app.use(function (req, res, next) {
